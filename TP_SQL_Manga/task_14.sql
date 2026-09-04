3fcaf0741c7de663322c5daad65439f248398333
Mathieu7483 <mathieu.godalier@orange.fr>
2026-09-04 09:39:06 +0200

    Add SQL query to calculate revenue by manga genre

--- TP_SQL_Manga/task_14.sql
+++ TP_SQL_Manga/task_14.sql
@@ -1,1 +1,7 @@
-SELECT
+SELECT gm.signification, ROUND(SUM(m.prix_base * tyl.coefficient), 2) AS chiffre_affaires
+FROM mangas m
+JOIN genres_manga gm ON m.code_genre = gm.code_genre
+JOIN table_location tl ON m.num_manga = tl.num_manga
+JOIN types_location tyl ON tl.code_type = tyl.code_type
+GROUP BY gm.signification
+ORDER BY chiffre_affaires DESC;
