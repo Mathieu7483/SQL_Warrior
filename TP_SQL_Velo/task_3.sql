4f121d65634e3ca4d5190bb5058f09f54d867ad2
Mathieu7483 <mathieu.godalier@orange.fr>
2026-09-04 11:05:02 +0200

    Add SQL script to demonstrate transaction handling with velos table

---
+++ TP_SQL_Velo/task_3.sql
@@ -0,0 +1,19 @@
+SELECT id, statut
+FROM velos
+WHERE id = 1;
+
+START TRANSACTION;
+
+UPDATE velos
+SET statut = 'maintenance'
+WHERE id = 1;
+
+SELECT id, statut
+FROM velos
+WHERE id = 1;
+
+ROLLBACK;
+
+SELECT id, statut
+FROM velos
+WHERE id = 1;
